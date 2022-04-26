5a402f60a6981483b9e3b114a2c492593e9b44e3
Jens Penneman <jenspenneman26@gmail.com>
2022-04-26 21:53:37 +0200

    Added some info about me in config.jsx
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

---
+++ src/config.jsx
@@ -0,0 +1,71 @@
+import JensPenneman from "./images/IMG_1429.jpeg";
+
+import HoGentLogo from "./images/HoGentLogo.png";
+import BroederschoolStekeneLogo from "./images/BroederschoolStekeneLogo.png";
+
+import {Facebook, Instagram, LinkedIn, Twitter} from "@mui/icons-material";
+
+const firstname = "Jens";
+const lastname  = "Penneman";
+
+const portret = JensPenneman;
+
+const studies = [
+	{
+		startdate:  new Date("2020-09-01"),
+		enddate:    new Date("2023-06-01"),
+		name:       "Toegepaste informatica",
+		href:       "https://www.hogent.be/opleidingen/bachelors/toegepaste-informatica/",
+		schoolName: "HoGent",
+		schoolHref: "https://www.hogent.be/",
+		schoolLogo: HoGentLogo,
+	},
+	{
+		startdate:  new Date("2018-09-01"),
+		enddate:    new Date("2020-06-01"),
+		name:       "I²CT",
+		href:       undefined,
+		schoolName: "GTI Beveren",
+		schoolHref: "https://www.beveren.be/nl/scholen/gti-beveren",
+		schoolLogo: undefined,
+	},
+	{
+		startdate:  new Date("2016-09-01"),
+		enddate:    new Date("2018-06-01"),
+		name:       "Electro mechanica",
+		href:       "https://stekene.broeders.be/studieaanbod/4de-leerjaar-d-a-finaliteit-domeingebonden-elektromechanische-technieken/",
+		schoolName: "Broederschool Stekene",
+		schoolHref: "https://stekene.broeders.be/",
+		schoolLogo: BroederschoolStekeneLogo,
+	},
+];
+
+const work = [{
+	startdate: new Date("2021-10-01"),
+	enddate:   undefined,
+	name:      "WEB4YOU",
+	role:      "Co-Founder",
+	href:      "https://web-4-you.be/",
+}/*De anderen moeten nog komen*/];
+
+const projects = [/*Nog geen toegevoegd*/];
+
+const socialMedia = [{
+	name: "Facebook",
+	icon: <Facebook />,
+	href: "https://facebook.com",
+}, {
+	name: "Instagram",
+	icon: <Instagram />,
+	href: "https://facebook.com",
+}, {
+	name: "Twitter",
+	icon: <Twitter />,
+	href: "https://facebook.com",
+}, {
+	name: "LinkedIn",
+	icon: <LinkedIn />,
+	href: "https://facebook.com",
+}];
+
+export {firstname, lastname, portret, studies, work, projects, socialMedia};
